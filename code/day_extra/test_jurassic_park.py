5db1b77c4a7ada732420ac65cba3c35620a5d9f9
Mikael Trellet <mikael.trellet@gmail.com>
2019-09-11 22:05:16 +0200

    Complete Jurassic park example

--- code/day_extra/test_jurassic_park.py
+++ code/day_extra/test_jurassic_park.py
@@ -2,7 +2,7 @@ import unittest
 from random import randrange
 
 
-from .jurassic_park import Dinosaur, Triceratops, TyrannosaurusRex
+from .jurassic_park import Dinosaur, Triceratops, TyrannosaurusRex, JurassicPark
 
 
 class TestJurassicPark(unittest.TestCase):
@@ -13,8 +13,37 @@ class TestJurassicPark(unittest.TestCase):
         self.assertNotEqual(dino.size, 200)
 
     def test_new_triceratops(self):
-        triceratops = Triceratops(randrange(5, 15))
+        triceratops = Triceratops(randrange(7, 10))
         self.assertTrue(5 <= triceratops.size <= 15)
         self.assertEqual(triceratops.roar(), 'buff! buff!')
 
-    # Your turn!!!
+    def test_new_trex(self):
+        trex = TyrannosaurusRex(randrange(5, 15))
+        self.assertTrue(5 <= trex.size <= 15)
+        self.assertEqual(trex.roar(), 'buff! buff!')
+
+    def test_park(self):
+        park = JurassicPark()
+        self.assertEqual(park._tyrannosaurus, [])
+        self.assertEqual(park._triceratops, [])
+
+        trex = TyrannosaurusRex(randrange(5, 15))
+        triceratops = Triceratops(randrange(7, 10))
+
+        park.add_trex(trex)
+
+        self.assertEqual(len(park._tyrannosaurus), 1)
+        self.assertEqual(len(park._triceratops), 0)
+
+        park.add_triceratops(triceratops)
+
+        self.assertEqual(len(park._tyrannosaurus), 1)
+        self.assertEqual(len(park._triceratops), 1)
+
+        park.battle()
+
+        self.assertEqual(len(park._tyrannosaurus), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
